caf9068cd30396c472690c6909dddd5b3d7939ad
Reid <Reid@pokersprout.com>
2017-05-18 12:53:09 -0400

    Data is scraped from product page. Data requires organization.
    Data is scraped from product page. Data requires organization.
    
    Instead of multiple X-ray calls, the application needs to make calls
    simultaneously or create synchronous data output mechanisms so that all
    product data is indexed in an organized way.

--- scraper.js
+++ scraper.js
@@ -1,21 +1,8 @@
 //If you’re shooting for the "Exceeds Expectations" grade, it is recommended that you mention so in your submission notes.
 
 
-//Create a scraper.js file that will contain your command line application.
-  //Done by making the .js file
-
-  //Choose and use two third-party npm packages that meet particular requirements.
-    //One package should be used to scrape content from the site.
-      //Chose X-ray to scrape content and as a module that has several downloads and recent updates
-
-    //The other package should create the CSV file.
-      //Chose json2csv to scrape content and as a module that has several downloads and recent updates
-
-//Your project should also include a package.json file that includes your project’s dependencies.
-  //Done by using npm init (dependencies properly saved to package.json with --save flag)
-
-//The npm install command should install your dependencies.
-  //Done by using the --save flag each time npm install <pkg> was used for the app
+//The other package should create the CSV file.
+  //Chose json2csv to scrape content and as a module that has several downloads and recent updates
 
 const Xray = require('x-ray');
 const json2csv = require('json2csv');
@@ -27,33 +14,24 @@ if (!fs.existsSync(dir)){
     fs.mkdirSync(dir);
 }
 
+//Storing asychronous and unorganized data that doesn't match by index
+let productPrice = [];
+let pageTitle = [];
+let productURL = [];
+let imageURL = [];
+
+let CSVData = [productPrice, pageTitle, productURL, imageURL];
+
 // Get the price, title, url and image url from the product page
 const x = Xray();
 const productPageURL = "http://shirts4mike.com/shirts.php";
 
-const shirtURL = "http://www.shirts4mike.com/shirt.php?id=101";
-
-//For each product, get the PRICE TITLE URL & IMAGEURL from the product page
-
-const title = x(productPageURL, {
-  // price: x(shirtURL, {price: '.price'}),
-  title: 'title'
-  // url: 'a'
-})(function(err, obj){
-  console.log(obj.title);
-
-  //On error
-    //If needed, convert error to user friendly message
-      //Display message to user (in console)
-    //Send err to error log file with time stamp
-})
 //WRITES OUTPUT TO JSON FILE AND THIS IS WHERE json2csv WOULD BE USED
 // .write('results.json');
 
 
-x(productPageURL, '.products a',
+x(productPageURL, '.products a', //NEED <TITLE> INNER TEXT FROM PRODUCT PAGE
   [{
-    // a: '',
     href: '@href',
     imageURL: 'img@src'
   }]
@@ -65,20 +43,35 @@ x(productPageURL, '.products a',
     //Send err to error log file with time stamp
 
   for(let i=0; i<obj.length; i++){
-    console.log(obj[i].href + " " + obj[i].imageURL);
+    console.log(obj[i].href + " " + obj[i].imageURL + " " + obj[i].productTitle);
+
+
     //  + "\n" + obj[i].price
     //prints http://shirts4mike.com/shirt.php?id=101,http://shirts4mike.com/shirt.php?id=102, etc.
 
-    x(obj[i].href, '.price')(function(err, obj){
+    x(obj[i].href,'.price')(function(err, obj){ //This should scrape both price and title to keep matched data
 
       //On error
         //If needed, convert error to user friendly message
           //Display message to user (in console)
         //Send err to error log file with time stamp
       console.log(obj);
+      productPrice.push(obj);
+      console.log(productPrice);
+    })
+
+    x(obj[i].href,'title')(function(err, obj){
+
+
+      console.log(obj + " title number " + (i+1));
+      pageTitle.push(obj);
+      console.log(pageTitle);
     })
   }
 })
+
+//Build arrays for each data type to be thrown into CSV
+
 
 
 // x(productPageURL, {
@@ -93,6 +86,8 @@ x(productPageURL, '.products a',
 // Export product data to CSV file in dir './data' in a particular order (see below)
   //The information should be stored in an CSV file that is named for the date it was created
   //e.g. 2016-11-21.csv
+    //Use built-in JS Date object and then parse the current date's year month
+      //and day to create the file name
 
 // Assume that the the column headers in the CSV need to be in a certain order
   // to be correctly entered into a database.
